b4f3a11a06ec701dc76ff97fa3750908994be9b5
Kenayred <99986813+Kenayred@users.noreply.github.com>
2025-10-04 10:14:36 -0600

    Update config.js
    Variables dinamicas

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import{config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || 'mysql.railway.internal'
-export const DB_PORT=process.env.DB_PORT || 3306
-export const DB_USER=process.env.DB_USER || 'root'
-export const DB_PASSWORD=process.env.DB_PASSWORD || 'szgJFkfmagMrDcQQsLgNURiNZlbqopzI'
-export const DB_DATABASE=process.env.DB_DATABASE || 'railway'
+export const DB_HOST=process.env.DB_HOST || ${{MySQL.MYSQLHOST}}
+export const DB_PORT=process.env.DB_PORT || ${{MySQL.MYSQLPORT}}
+export const DB_USER=process.env.DB_USER || ${{MySQL.MYSQLUSER}}
+export const DB_PASSWORD=process.env.DB_PASSWORD || ${{MySQL.MYSQL_ROOT_PASSWORD}}
+export const DB_DATABASE=process.env.DB_DATABASE || ${{MySQL.MYSQL_DATABASE}}
