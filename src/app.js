aaac690aac5227dd03429a0e666eb73e79a1a421
Kenayred <99986813+Kenayred@users.noreply.github.com>
2025-10-08 07:54:49 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
-import cors from "cors';
 import marketRoutes from './routes/market.routes.js'
 
 const app=express()
 // En tu API (Node.js/Express)
-app.use(cors({
-  origin: '*', // O especifica tu dominio
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
 // Middleware para procesar JSON
 app.use(express.json());
 
